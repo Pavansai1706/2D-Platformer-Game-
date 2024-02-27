a744b1115462b8109f800cefa8c63a150d5cc672
pavansai <thammupavan2002@gmail.com>
2024-02-27 13:03:20 +0530

    Collectible assignment

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object has a PlayerController component
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+
+        if (playerController != null)
+        {
+            playerController.PickUpKey();
+            Destroy(gameObject);
+        }
+    }
+}
+
+
