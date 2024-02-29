c06f908e5a66dafe19cc9b06f8ad66f3828558ba
pavansai <thammupavan2002@gmail.com>
2024-02-29 14:31:51 +0530

    Game over ui Assignment

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    public GameObject gameOverObject; // Reference to the game over object
+
+    public void ToggleGameOverObject(bool isActive)
+    {
+        // Activate or deactivate the game over object based on the parameter
+        gameOverObject.SetActive(isActive);
+    }
+    public Button buttonRestart;
+
+    public static object Instance { get; internal set; }
+
+    private void Awake()
+    {
+        buttonRestart.onClick.AddListener(ReloadLevel);
+    }
+    public void PlayerDied()
+    {
+        gameObject.SetActive(true);
+
+    }
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
