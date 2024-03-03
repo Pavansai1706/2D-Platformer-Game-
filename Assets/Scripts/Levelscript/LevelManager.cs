5dfa1b2b14c7508cafd41c11953f600c0cf29d97
pavansai <thammupavan2002@gmail.com>
2024-03-03 12:57:21 +0530

    Level Lock Assignment

---
+++ Assets/Scripts/Levelscript/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager instance;
+    public static LevelManager Instance { get { return instance; } }
+
+    public string LevelName;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        if (GetLevelStatus(LevelName) == LevelStatus.Locked)
+            {
+            SetLevelStatus(LevelName, LevelStatus.Unlocked);
+        }
+    }
+
+    public LevelStatus GetLevelStatus(string level)
+    {
+        LevelStatus levelStatus = (LevelStatus)PlayerPrefs.GetInt(level, 0);
+        return levelStatus;
+    }
+    public void SetLevelStatus(string level, LevelStatus levelStatus)
+    {
+        PlayerPrefs.SetInt(level, (int)levelStatus);
+    }
+}
